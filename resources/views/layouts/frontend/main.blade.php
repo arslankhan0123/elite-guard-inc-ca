8b06d3bfae2d337818ce6f6ad6892aad386d39c4
arslankhan0123 <arslan.devsspace@gmail.com>
2026-09-08 22:27:46 +0500

    working on facebook post data

--- resources/views/layouts/frontend/main.blade.php
+++ resources/views/layouts/frontend/main.blade.php
@@ -11,8 +11,8 @@
   <meta name="robots" content="noindex, nofollow">
 
   <!-- Favicons -->
-  <link href="{{asset('frontend/assets/img/favicon.png')}}" rel="icon">
-  <link href="{{asset('frontend/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+  <link href="{{asset('frontend/assets/img/logo.png')}}" rel="icon" type="image/png">
+  <link href="{{asset('frontend/assets/img/logo.png')}}" rel="apple-touch-icon">
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
