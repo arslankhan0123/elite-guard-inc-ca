eaa11ac22ba0477218c7a1cebc3f98f052453e21
arslankhan0123 <arslan.devsspace@gmail.com>
2026-09-23 22:39:11 +0500

    working on settings

--- resources/views/frontend/careers/index.blade.php
+++ resources/views/frontend/careers/index.blade.php
@@ -272,6 +272,13 @@
         }
 
         /* Hiring Process */
+        @media (min-width: 992px) {
+            .col-lg-15 {
+                flex: 0 0 auto;
+                width: 20%;
+            }
+        }
+
         .process-step {
             text-align: center;
             padding: 20px;
@@ -671,35 +678,35 @@
                         <h2 class="section-heading">Our Hiring Process</h2>
                     </div>
 
-                    <div class="col-lg-15 col-md-3 col-sm-6 mb-4" style="flex: 0 0 auto; width: 20%;">
+                    <div class="col-lg-15 col-md-4 col-sm-6 col-12 mb-4">
                         <div class="process-step">
                             <div class="process-icon"><i class="fa-solid fa-file-signature" style="color: #b8860b;"></i></div>
                             <h4 class="process-title">1. Apply</h4>
                             <p class="process-text">Submit your application</p>
                         </div>
                     </div>
-                    <div class="col-lg-15 col-md-3 col-sm-6 mb-4" style="flex: 0 0 auto; width: 20%;">
+                    <div class="col-lg-15 col-md-4 col-sm-6 col-12 mb-4">
                         <div class="process-step">
                             <div class="process-icon"><i class="fa-solid fa-user-check" style="color: #b8860b;"></i></div>
                             <h4 class="process-title">2. Review</h4>
                             <p class="process-text">We review your information</p>
                         </div>
                     </div>
-                    <div class="col-lg-15 col-md-3 col-sm-6 mb-4" style="flex: 0 0 auto; width: 20%;">
+                    <div class="col-lg-15 col-md-4 col-sm-6 col-12 mb-4">
                         <div class="process-step">
                             <div class="process-icon"><i class="fa-solid fa-comments" style="color: #b8860b;"></i></div>
                             <h4 class="process-title">3. Interview</h4>
                             <p class="process-text">Initial interview (virtual/in-person)</p>
                         </div>
                     </div>
-                    <div class="col-lg-15 col-md-3 col-sm-6 mb-4" style="flex: 0 0 auto; width: 20%;">
+                    <div class="col-lg-15 col-md-4 col-sm-6 col-12 mb-4">
                         <div class="process-step">
                             <div class="process-icon"><i class="fa-solid fa-shield-halved" style="color: #b8860b;"></i></div>
                             <h4 class="process-title">4. Verification</h4>
                             <p class="process-text">Licence and reference checks</p>
                         </div>
                     </div>
-                    <div class="col-lg-15 col-md-3 col-sm-6 mb-4" style="flex: 0 0 auto; width: 20%;">
+                    <div class="col-lg-15 col-md-4 col-sm-6 col-12 mb-4">
                         <div class="process-step">
                             <div class="process-icon"><i class="fa-solid fa-handshake" style="color: #b8860b;"></i></div>
                             <h4 class="process-title">5. Welcome</h4>
